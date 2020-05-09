11c678eeeb77a61052a9778c4cac1b59c69e5b50
Paul Milbank <paulmilbank@gmail.com>
2020-05-09 22:03:41 +1200

    dump a mvc project and test controller

---
+++ src/license_api/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using license_api.Models;
+using System.Text.Encodings.Web;
+
+namespace license_api.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index () {
+            _logger.LogDebug("Hit the index route on the test controller");
+            return "Hello World";
+        }
+
+        public string Hello (string Name) {
+            _logger.LogDebug($"Hit the Hello world route with parameter {Name}");
+            return HtmlEncoder.Default.Encode($"Hello {Name}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            _logger.LogError("Oh No.");
+            return View(new ErrorViewModel() { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
