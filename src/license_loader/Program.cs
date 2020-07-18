32a893c75efd76fe2ca0dcb99bb923b6b7af74f7
Paul Milbank <paul@pop-os.localdomain>
2020-07-18 21:29:44 +1200

    create a new data class and move the license class into it

--- src/license_loader/Program.cs
+++ src/license_loader/Program.cs
@@ -15,16 +15,4 @@ namespace license_loader
             Console.WriteLine(licenses[0].conditions[0]);
         }
     }
-
-    class license 
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string licenseText { get; set; }
-        public string url { get; set; }
-        public string description { get; set; }
-        public List<string> permissions { get; set; }
-        public List<string> conditions { get; set; }
-        public List<string> limitations { get; set; }
-    }
 }
